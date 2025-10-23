b8009a3e37813f618895b7cdced8558b2c933790
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-10-23 14:30:50 -0300

    feat: adiciona React Router e páginas iniciais (Login, Register, Dashboard, Home)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
+import { React, StrictMode } from 'react'
+import { ReactDOM, createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { AuthProvider } from './contexts/AuthContext.tsx'
+import './styles/index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 )

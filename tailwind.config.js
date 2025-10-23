4de8d983a8296c16ff976a6d488b9c2e8d4704dc
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-10-23 10:40:20 -0300

    style: Define estilos e padrões que serão usados na NexusVault

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0B132B",
+        secondary: "#1C2541",
+        accent: "#5BC0BE",
+        text: "#E0E1DD",
+      },
+    },
   },
   plugins: [],
 }
